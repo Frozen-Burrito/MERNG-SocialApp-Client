465a4b93ad21785f0ff7722273b5d0896533f93d
FrozenBurrito <f3rm3ndouza@outlook.com>
2020-10-29 09:06:14 -0600

    Setup proxy and edit base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -10,7 +10,7 @@ import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://sheltered-brushlands-24132.herokuapp.com/'
 }); 
 
 const authLink = setContext(() => {
